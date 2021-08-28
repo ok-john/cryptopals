863a9e8c58ca80b8aa9317a8b175b4af588833aa
reflect <reflect>
2021-08-28 01:43:46 -0400

    challenge 1 & 2

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/ca-std/lib"
+)
 
 func main() {
-	fmt.Sprintln("hello there")
+	challenge1()
+	challenge2()
+}
+
+func challenge2() {
+	out(lib.XorY(<-lib.DecodeHex([]byte("1c0111001f010100061a024b53535009181c")), <-lib.DecodeHex([]byte("686974207468652062756c6c277320657965"))))
+}
+
+func challenge1() {
+	out(<-lib.DecodeHex([]byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))
+}
+
+func out(b []byte) {
+	os.Stdout.Write([]byte("\n"))
+	os.Stdout.Write(b)
+	os.Stdout.Write([]byte("\n"))
 }
